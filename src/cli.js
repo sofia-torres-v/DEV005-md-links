9b5807adc80933c83e854820f2439f4b5a428b8c
sofiauldemiaclear <sofy.torresv@gmail.com>
2023-06-01 10:04:40 -0500

    ADD:Colors to the command line interface

--- src/cli.js
+++ src/cli.js
@@ -35,10 +35,26 @@ mdLinks(userPath, optionsObj)
       const statsTotal = statsBroken(res); // This function returns an object
       console.log(`Total: ${statsTotal.Total} Unique: ${statsTotal.Uniques} ${statsTotal.Broken ? `Broken: ${statsTotal.Broken}`.red : ''} `.blue);
 
-    }else{
-      console.log(res);
-    }
-  })
+    }else if (optionsObj.validate && !optionsObj.stats){
+        res.forEach((element) => {
+          console.log(`URL: ${element.href}`.blue);
+          console.log(`Text: ${element.text}`.grey);
+          console.log(`File: ${element.file}`.blue);
+          console.log(`Status: ${element.status}`.yellow);
+          console.log(`Status Text: ${element.statusText}`.green);
+          console.log('');
+        })
+        // console.log('Result:', res);
+      } else {
+        res.forEach((element) => {
+          console.log(`URL: ${element.href}`.blue);
+          console.log(`Text: ${element.text}`.grey);
+          console.log(`File: ${element.file}`.blue);
+          console.log('');
+        })
+      }
+    })
+
   .catch((error) => {
     console.log(`Error: ${error}`);
     return error;
